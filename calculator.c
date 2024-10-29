34e0370f15856c9c698cc130e8b2ec84be178e0c
Ayan007OP-coder <de.ayan05@gmail.com>
2024-10-29 19:56:13 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
 int main()
-{ int a,b;
-printf("enter the two integers :");
-scanf("%d %d",&a,&b);
+{ float a,b;
+printf("enter the two numbers :");
+scanf("%f %f",&a,&b);
 int x;
 printf("enter 1,2,3,4 for multiplication,division,addition,substraction respectively : ");
 scanf("%d",&x);
 switch(x)
 {
-case 1 : printf("%d\n",a*b);
+case 1 : printf("%f\n",a*b);
 break;
-case 2 : printf("%d\n",a/b);
+case 2 : printf("%f\n",a/b);
 break;
-case 3 : printf("%d\n",a+b);
+case 3 : printf("%f\n",a+b);
 break;
-case 4 : printf("%d\n",a-b);
+case 4 : printf("%f\n",a-b);
 break;
 default : printf("not valid\n");}
     return 0;
